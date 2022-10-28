62c046be153897ed15224d9becf916bc72d7ee29
dashpen <dashpenning@gmail.com>
2022-10-28 14:38:30 -0700

    api fix (now works!). added globals wins/losses

--- test.py
+++ test.py
@@ -9,16 +9,27 @@ app_api2 = Blueprint('api', __name__,
 
 # API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
 api = Api(app_api2)
+data = [{"Wins": 0, "Losses": 0}]
 
 class TestAPI:
-    class _GetThing(Resource):
+    class _GetAll(Resource):
         def get(self):
-            return jsonify([1, 2, 3])
+            return jsonify(data)
+    class _PutWin(Resource):
+        def put(self):
+            data[0]["Wins"] = data[0]["Wins"] + 1;
+            return jsonify(data[0])
+    class _PutLoss(Resource):
+        def put(self):
+            data[0]["Losses"] = data[0]["Losses"] + 1;
+            return jsonify(data[0])
 
 
-    api.add_resource(_GetThing, '/')
+    api.add_resource(_GetAll, '/')
+    api.add_resource(_PutWin, '/win')
+    api.add_resource(_PutLoss, '/loss')
 
 if __name__ == "__main__": 
-    # server = "http://127.0.0.1:5000"
-    server = 'https://swag.nighthawkcodingteams.cf'
+    # server = "http://127.0.0.1:5000" #local
+    server = 'https://swag.nighthawkcodingteams.cf' #web
     url = server + "/api/tester"
